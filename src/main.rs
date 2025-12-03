fff95b3f60d1ff9811aabc0f214aadcc9d39013c
Qiqianily <Joedian@163.com>
2025-12-03 09:56:38 +0800

    feat: finished customed response errors.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,18 @@
-fn main() -> anyhow::Result<()> {
-    println!("This is a backend project template for Rust!");
+use todo_list_v1::response::ApiResult;
+use todo_list_v1::response::resp::ApiResponse;
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    // build our application with a route
+    let app = axum::Router::new().route("/", axum::routing::get(get_current_version_handler));
+
+    // run our app with hyper, listening globally on port 3000
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    axum::serve(listener, app).await.unwrap();
     Ok(())
+}
+
+pub async fn get_current_version_handler() -> ApiResult<ApiResponse<String>> {
+    // tracing::info!("Querying current version ...");
+    // return the current version
+    Ok(ApiResponse::success("v0.1.1".into()))
 }
